bf27bd6e3c4f0d1c417d7b89ad959c320fd8b2c6
JonnyOThan <jonnyothan@gmail.com>
2023-12-04 08:58:04 -0500

    bump version to 3.0.2

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1")]
-[assembly: AssemblyFileVersion("3.0.1")]
+[assembly: AssemblyVersion("3.0.2")]
+[assembly: AssemblyFileVersion("3.0.2")]
 
 [assembly: KSPAssembly("Scale", 3, 0)]
 [assembly: KSPAssemblyDependencyEqualMajor("Scale_Redist", 3, 0)]
