6db4cdbae970b5c9168fd29d71a6dffacff9138f
JonnyOThan <jonnyothan@gmail.com>
2024-02-14 08:06:21 -0500

    bump version to 3.2.2

--- Source/Properties/SharedAssemblyInfo.cs
+++ Source/Properties/SharedAssemblyInfo.cs
@@ -14,7 +14,7 @@ namespace TweakScale
 	{
 		public const int MAJOR = 3;
 		public const int MINOR = 2;
-		public const int REVISION = 1;
-		public const string STRING = "3.2.1";
+		public const int REVISION = 2;
+		public const string STRING = "3.2.2";
 	}
 }
