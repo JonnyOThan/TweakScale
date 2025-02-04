575275d0e4e8b022f771403a7b02d92145bf60a7
JonnyOThan <jonnyothan@gmail.com>
2025-02-04 11:07:40 -0500

    switch projects to sdk-style

--- Source/Scale/Properties/AssemblyInfo.cs
+++ Source/Scale/Properties/AssemblyInfo.cs
@@ -6,8 +6,6 @@ using System.Security;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,25 +17,3 @@ using System.Security;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("99eed0a4-ba83-42d9-80bd-0591cf3840be")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: KSPAssemblyDependencyEqualMajor("Scale_Redist", TweakScale.VersionInfo.MAJOR, TweakScale.VersionInfo.MINOR, TweakScale.VersionInfo.REVISION)]
-[assembly: KSPAssemblyDependency("HarmonyKSP", 1, 0)]
-[assembly: KSPAssemblyDependency("ModuleManager", 2, 5)]
-
-namespace TweakScale
-{
-	static partial class VersionInfo
-	{
-		public const string ASSEMBLY = "Scale";
-	}
-}
